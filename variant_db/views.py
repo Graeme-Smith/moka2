5eaad961df06677aa22cee24a88e81fa3dec0f89
Erik <erikwaskiewicz@gmail.com>
2020-02-27 10:30:58 +0000

    Make templates for home, view and manual import pages

--- variant_db/views.py
+++ variant_db/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'variant_db/home.html', {})
+
+
+def view(request):
+    return render(request, 'variant_db/view.html', {})
+
+
+def manual_import(request):
+    return render(request, 'variant_db/manual_import.html', {})
