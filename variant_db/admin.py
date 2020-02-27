b10fa26beedbfc307e1a76f659802d842a2a2be0
Erik <erikwaskiewicz@gmail.com>
2020-02-27 15:12:17 +0000

    Updated admin display

--- variant_db/admin.py
+++ variant_db/admin.py
@@ -1,12 +1,41 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Patient)
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'surname')
+admin.site.register(Patient, PatientAdmin)
+
 admin.site.register(Family)
-admin.site.register(dnaSample)
-admin.site.register(Test)
-admin.site.register(Variant)
-admin.site.register(Gene)
-admin.site.register(Classification)
-admin.site.register(Evidence)
-admin.site.register(acmgCodes)
+
+class PhenotypeAdmin(admin.ModelAdmin):
+    list_display = ('visit_id', 'visit_date', 'patient_id', 'description', 'stage')
+admin.site.register(Phenotype, PhenotypeAdmin)
+
+class dnaSampleAdmin(admin.ModelAdmin):
+    list_display = ('sample_id', 'patient_id')
+admin.site.register(dnaSample, dnaSampleAdmin)
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('test_id', 'test_type')
+admin.site.register(Test, TestAdmin)
+
+class VariantAdmin(admin.ModelAdmin):
+    list_display = ('variant_id', 'coords_cdna', 'coords_protein', 'coords_genomic')
+admin.site.register(Variant, VariantAdmin)
+
+class GeneAdmin(admin.ModelAdmin):
+    list_display = ('gene_id', 'gene_name')
+admin.site.register(Gene, GeneAdmin)
+
+class ClassificationAdmin(admin.ModelAdmin):
+    list_display = ('classification_id', 'variant_id', 'classification')
+admin.site.register(Classification, ClassificationAdmin)
+
+class EvidenceAdmin(admin.ModelAdmin):
+    list_display = ('evidence_id', 'classification_id', 'code_id')
+admin.site.register(Evidence), EvidenceAdmin
+
+class acmgCodesAdmin(admin.ModelAdmin):
+    list_display = ('code_id', 'description')
+admin.site.register(acmgCodes, acmgCodesAdmin)
